b471356cffd8272e3f73a5c5c5892639bab0b79d
vadym-usatiuk <vadym.usatiuk@gmail.com>
2020-08-20 13:55:27 -0400

    done

---
+++ Day-1-Data-Types/src/main/java/Solution.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Main {
+
+    public static void main(String[] args){
+
+}
+
+    }
+
